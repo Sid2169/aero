b66cd0021cdbbc1c6d6e5a8d315d11c849e22170
Sid2169 <siddharthasingh21109572@gmail.com>
2025-11-30 16:27:47 +0530

    refactor: rename component.js to WeatherApi.js for better clarity
    - Rename src/components/component.js to src/components/WeatherApi.js
    - Update import statements in index.js to reflect new filename
    - Improve codebase organization and naming conventions
    - Align file names with their actual functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-import Container from "./components/component";
+import Container from "./components/WeatherApi";
 
 // Delete this code and write your own
 document.querySelector("#root").appendChild(Container());
